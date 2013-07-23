699f82ba2d7a15e69d4c790d6f28cefbf52f03e7
Mikael Jorhult <mikael@jorhult.se>
2013-07-23 15:22:06 +0200

    Adhere to coding conventions.

--- main.js
+++ main.js
@@ -2,53 +2,55 @@
 /*global define, $, brackets */
 
 /** Brackets Todos Extension 
-	description 
+    description 
 */
-define( function( require, exports, module ) {
-	'use strict';
-	
-	var CommandManager = brackets.getModule( 'command/CommandManager' ),
-		Menus = brackets.getModule( 'command/Menus' ),
-		DocumentManager = brackets.getModule( 'document/DocumentManager' );
-	//var EditorManager  = brackets.getModule("editor/EditorManager");
-	//var ProjectManager = brackets.getModule("project/ProjectManager");
-	//var FileUtils = brackets.getModule("file/FileUtils");
-	//var NativeApp = brackets.getModule("utils/NativeApp");
-	//var Commands = brackets.getModule("command/Commands");
-	
-	var COMMAND_ID = 'mikaeljorhult.bracketsTodo.parseTodo',
-		MENU_NAME = 'Todo',
-		regex = {
-			prefix: '(?:\/\*\s*|\/\/\s*)(',
-			suffix: '):\ *.*(?=\n+)',
-			keywords: [ 'TODO', 'NOTE', 'FIX\s?ME', 'CHANGES' ]
-		};
-	
-	// Basic functionality.
-	function parseTodo() {
-		var currentDoc = DocumentManager.getCurrentDocument(),
-			documentText,
-			expression,
-			todos;
-		
-		// Check for open documents.
-		if ( currentDoc !== null ) {
-			expression = new RegExp( regex.prefix + regex.keywords.join( '|' ) + regex.suffix, 'gi' );
-			documentText = currentDoc.getText();
-			todos = documentText.match( expression );
-			
-			// Go through each todo.
-			for ( var i = 0; i < todos.length; i++ ) {
-				console.log( todos[ i ] );
-			}
-		}
-	}
-	
-	// Register extension.
-	CommandManager.register( MENU_NAME, COMMAND_ID, parseTodo );
-	
-	// Add command to menu.
-	var menu = Menus.getMenu( Menus.AppMenuBar.FILE_MENU );
-	menu.addMenuDivider();
-	menu.addMenuItem( COMMAND_ID );
-} );
+define(function (require, exports, module) {
+    'use strict';
+    
+    var CommandManager = brackets.getModule("command/CommandManager"),
+        Menus = brackets.getModule("command/Menus"),
+        DocumentManager = brackets.getModule("document/DocumentManager");
+    //var EditorManager  = brackets.getModule("editor/EditorManager");
+    //var ProjectManager = brackets.getModule("project/ProjectManager");
+    //var FileUtils = brackets.getModule("file/FileUtils");
+    //var NativeApp = brackets.getModule("utils/NativeApp");
+    //var Commands = brackets.getModule("command/Commands");
+    
+    var COMMAND_ID = "mikaeljorhult.bracketsTodo.parseTodo",
+        MENU_NAME = "Todo",
+        regex = {
+            prefix: "(?:\/\*\s*|\/\/\s*)(",
+            suffix: "):\ *.*(?=\n+)",
+            keywords: [ "TODO", "NOTE", "FIX\s?ME", "CHANGES" ]
+        };
+    
+    /** Annotation 
+        description 
+	*/
+    function parseTodo() {
+        var currentDoc = DocumentManager.getCurrentDocument(),
+            documentText,
+            expression,
+            todos;
+        
+        // Check for open documents.
+        if (currentDoc !== null) {
+            expression = new RegExp(regex.prefix + regex.keywords.join("|") + regex.suffix, "gi");
+            documentText = currentDoc.getText();
+            todos = documentText.match(expression);
+            
+            // Go through each todo.
+            todos.forEach(function (value, index) {
+                console.log(value);
+            });
+        }
+    }
+    
+    // Register extension.
+    CommandManager.register(MENU_NAME, COMMAND_ID, parseTodo);
+    
+    // Add command to menu.
+    var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
+    menu.addMenuDivider();
+    menu.addMenuItem(COMMAND_ID, "Ctrl-Alt-T");
+});
