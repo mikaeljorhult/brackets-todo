011e2d74630927f424ba4612d116955bdef19c1c
Mikael Jorhult <mikael@jorhult.se>
2013-11-20 12:17:08 +0100

    Split preferences and settings tests into separate it statements.

--- unittests.js
+++ unittests.js
@@ -1,13 +1,23 @@
+/* global brackets, define, describe, it, expect, jasmine */
+
 define( function( require, exports, module ) {
 	'use strict';
 	
+	// Get dependencies.
 	var Defaults = require( 'modules/Defaults' );
 	
 	describe( 'Todo', function() {
 		describe( 'DefaultsTest', function() {
+			// Test for default preferences.
 			it( 'should expose defaultPreferences variable', function() {
 				expect( Defaults.defaultPreferences ).not.toBeNull();
+				expect( Defaults.defaultPreferences ).toEqual( jasmine.any( Object ) );
+			} );
+			
+			// Test for default preferences.
+			it( 'should expose defaultSettings variable', function() {
 				expect( Defaults.defaultSettings ).not.toBeNull();
+				expect( Defaults.defaultSettings ).toEqual( jasmine.any( Object ) );
 			} );
 		} );
 	} );
