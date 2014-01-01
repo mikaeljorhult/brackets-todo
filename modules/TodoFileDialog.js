b4a77a0ddea95f8a73cb3a4996c74ab711082645
Mikael Jorhult <mikael@jorhult.se>
2014-01-01 21:28:25 +0100

    Add localized strings to file dialog.

--- modules/TodoFileDialog.js
+++ modules/TodoFileDialog.js
@@ -18,7 +18,7 @@ define( function ( require, exports, module ) {
 	function showDialog() {
 		var dialog = Dialogs.showModalDialogUsingTemplate( 
 			Mustache.render( todoFileDialogTemplate, {
-				
+				strings: Strings
 			} )
 		);
 		
--- nls/sv/Strings.js
+++ nls/sv/Strings.js
@@ -1,4 +1,13 @@
 define( {
+	// GENERAL.
+	YES: "Ja",
+	NO:  "Nej",
+	
+	// TOOLBAR.
 	EXPAND_ALL:    "Expandera alla",
-	COLLAPSE_ALL:  "Minimera alla"
+	COLLAPSE_ALL:  "Minimera alla",
+	
+	// FILE DIALOG.
+	FILE_DIALOG_TITLE:   "Skapa filen .todo?",
+	FILE_DIALOG_MESSAGE: "Det nuvarande projektet har ingen fil med namnet .todo. Vill du skapa en?"
 } );
