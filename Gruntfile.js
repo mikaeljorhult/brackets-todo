ac604fc7fce997090cd60b9648762f910e809c44
Mikael Jorhult <mikael@jorhult.se>
2014-01-23 21:59:07 +0100

    Use Grunt to run JSHint.

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function( grunt ) {
+	'use strict';
+	
+	// Require all Grunt tasks.
+	require( 'load-grunt-tasks' )( grunt );
+	
+	// Configure each task.
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( 'package.json' ),
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'main.js',
+				'unittests.js',
+				'modules/*.js'
+			]
+		}
+	} );
+	
+	// Register tasks.
+	grunt.registerTask( 'default', [ 'jshint' ]);
+};
