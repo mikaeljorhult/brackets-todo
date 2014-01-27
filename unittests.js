d80fccd457b9dd598c8600a65a3116cccf1cfe5c
Mikael Jorhult <mikael@jorhult.se>
2014-01-27 18:20:24 +0100

    Unit tests for SettingsManager and FileManager.

--- unittests.js
+++ unittests.js
@@ -11,7 +11,9 @@ define( function( require, exports, module ) {
 	
 		// Get Todo modules.
 		Defaults = require( 'modules/Defaults' ),
+		FileManager = require( 'modules/FileManager' ),
 		ParseUtils = require( 'modules/ParseUtils' ),
+		SettingsManager = require( 'modules/SettingsManager' ),
 	
 		// Setup paths and other variables.
 		extensionPath = FileUtils.getNativeModuleDirectoryPath( module ),
@@ -121,6 +123,87 @@ define( function( require, exports, module ) {
 					expect( files[ 0 ].todos ).toEqual( jasmine.any( Array ) );
 					expect( files[ 0 ].todos.length ).toEqual( 8 );
 				} );
+			} );
+		} );
+		
+		// Test module handling settings.
+		describe( 'SettingsManagerTest', function() {
+			// Test for settings getter.
+			it( 'should expose getSettings method', function() {
+				expect( SettingsManager.getSettings ).not.toBeNull();
+				expect( SettingsManager.getSettings ).toEqual( jasmine.any( Function ) );
+			} );
+			
+			// Test for settings setter.
+			it( 'should expose setSettings method', function() {
+				expect( SettingsManager.setSettings ).not.toBeNull();
+				expect( SettingsManager.setSettings ).toEqual( jasmine.any( Function ) );
+			} );
+			
+			// Test for method to merge settings.
+			it( 'should expose mergeSettings method', function() {
+				expect( SettingsManager.mergeSettings ).not.toBeNull();
+				expect( SettingsManager.mergeSettings ).toEqual( jasmine.any( Function ) );
+			} );
+			
+			// Test setting settings.
+			it( 'should save settings when set', function() {
+				var settings,
+					newSettings = {
+						test: 'value'
+					};
+				
+				// Merge and get settings.
+				SettingsManager.setSettings( newSettings );
+				settings = SettingsManager.getSettings();
+				
+				// Make sure that on prefix, not suffix, vaue has been merged.
+				expect( settings ).toEqual( jasmine.any( Object ) );
+				expect( settings.test ).toEqual( 'value' );
+			} );
+			
+			// Owerwriting tags.
+			it( 'should overwrite only supplied settings when merging', function() {
+				var settings,
+					userSettings = {
+						regex: {
+							prefix: 'TESTVALUE'
+						}
+					};
+				
+				// Merge and get settings.
+				SettingsManager.mergeSettings( userSettings );
+				settings = SettingsManager.getSettings();
+				
+				// Make sure that on prefix, not suffix, vaue has been merged.
+				expect( settings.regex ).toEqual( jasmine.any( Object ) );
+				expect( settings.regex.prefix ).not.toBeNull();
+				expect( settings.regex.suffix ).not.toBeNull();
+				expect( settings.regex.prefix ).toEqual( 'TESTVALUE' );
+			} );
+			
+			// Owerwriting tags.
+			it( 'should overwrite tags with user supplied tags', function() {
+				var settings,
+					userSettings = {
+						tags: [ 'TESTTAG' ]
+					};
+				
+				// Merge and get settings.
+				SettingsManager.mergeSettings( userSettings );
+				settings = SettingsManager.getSettings();
+				
+				expect( settings.tags ).toEqual( jasmine.any( Array ) );
+				expect( settings.tags.length ).toEqual( 1 );
+			} );
+		} );
+		
+		// Test module managing files to parse.
+		describe( 'FileManagerTest', function() {
+			// Test for file getter.
+			it( 'should expose getFiles method', function() {
+				expect( FileManager.getFiles ).not.toBeNull();
+				expect( FileManager.getFiles ).toEqual( jasmine.any( Function ) );
 			} );
 		} );
 	} );
