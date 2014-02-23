f9da9681111c709d89188295543331229f240159
Mikael Jorhult <mikael@jorhult.se>
2014-02-23 22:11:37 +0100

    Strings for settings dialog.

--- nls/root/Strings.js
+++ nls/root/Strings.js
@@ -3,8 +3,10 @@ define( {
 	EXTENSION_NAME: "Todo",
 	
 	// GENERAL.
-	YES: "Yes",
-	NO:  "No",
+	YES:    "Yes",
+	NO:     "No",
+	OK:     "Ok",
+	CANCEL: "Cancel",
 	
 	// TOOLBAR.
 	CONFIGURE:     "Configure",
@@ -14,5 +16,9 @@ define( {
 	
 	// FILE DIALOG.
 	FILE_DIALOG_TITLE:   "Create .todo file?",
-	FILE_DIALOG_MESSAGE: "The current project folder does not contain a .todo file. Would you like to create it?"
+	FILE_DIALOG_MESSAGE: "The current project folder does not contain a .todo file. Would you like to create it?",
+	
+	// SETTINGS DIALOG.
+	SETTINGS_DIALOG_TITLE:        "Todo Settings",
+	SETTINGS_DIALOG_REGEX_PREFIX: "RegExp prefix"
 } );
