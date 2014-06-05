53369602020aa2fb0e7dc3ad333f9b458ad919e7
Wei Lin <anzhihun@gmail.com>
2014-06-05 17:15:53 +0800

    Set indentation to 8 spaces

--- modules/SettingsDialog.js
+++ modules/SettingsDialog.js
@@ -1,119 +1,119 @@
 define(function (require, exports) {
-    'use strict';
+        'use strict';
 
-    // Get module dependencies.
-    var Dialogs = brackets.getModule('widgets/Dialogs'),
+        // Get module dependencies.
+        var Dialogs = brackets.getModule('widgets/Dialogs'),
+            
+            // Extension modules.
+            Defaults = require('modules/Defaults'),
+            Strings = require('modules/Strings'),
+            settingsDialogTemplate = require('text!../html/dialog-settings.html'),
 
-        // Extension modules.
-        Defaults = require('modules/Defaults'),
-        Strings = require('modules/Strings'),
-        settingsDialogTemplate = require('text!../html/dialog-settings.html'),
+            // Variables.
+            dialog;
 
-        // Variables.
-        dialog;
+        /**
+         * get each value of the preferences in dialog.
+         */
+        function getValues($dialog) {
 
-    /**
-     * get each value of the preferences in dialog.
-     */
-    function getValues($dialog) {
+                return {
+                        regex: {
+                                prefix: $dialog.find('#todo-settings-regex-prefix').val(),
+                                suffix: $dialog.find('#todo-settings-regex-suffix').val(),
+                        },
+                        tags: splitByComma($dialog.find('#todo-settings-tags').val()),
+                case : $dialog.find('#todo-settings-case-yes')[0].checked,
+                search: {
+                                scope: $dialog.find('#todo-settings-scope-file')[0].checked ? Defaults.CURRENT_SCOPE : Defaults.PROJECT_SCOPE,
+                                excludeFolders: splitByComma($dialog.find('#todo-settings-exclude-folders').val()),
+                                excludeFiles: splitByComma($dialog.find('#todo-settings-exclude-files').val())
+                        }
+                };
+        }
 
-        return {
-            regex: {
-                prefix: $dialog.find('#todo-settings-regex-prefix').val(),
-                suffix: $dialog.find('#todo-settings-regex-suffix').val(),
-            },
-            tags: splitByComma($dialog.find('#todo-settings-tags').val()),
-            case :$dialog.find('#todo-settings-case-yes')[0].checked,
-            search: {
-                scope: $dialog.find('#todo-settings-scope-file')[0].checked ? Defaults.CURRENT_SCOPE : Defaults.PROJECT_SCOPE,
-                excludeFolders: splitByComma($dialog.find('#todo-settings-exclude-folders').val()),
-                excludeFiles: splitByComma($dialog.find('#todo-settings-exclude-files').val())
-            }
+        /**
+         * check or uncheck radio button
+         */
+        function checkRadio(elementId, isChecked) {
+                $('#' + elementId).attr('checked', isChecked);
+        }
+
+        /**
+         * Initialize dialog values.
+         */
+        function initValues(settings) {
+                $('#todo-settings-regex-prefix').val(settings.regex.prefix);
+                $('#todo-settings-regex-suffix').val(settings.regex.suffix);
+                $('#todo-settings-tags').val(settings.tags.join(', '));
+                if (settings.case === true) {
+                        checkRadio('todo-settings-case-yes', true);
+                        checkRadio('todo-settings-case-no', false);
+                } else {
+                        checkRadio('todo-settings-case-yes', false);
+                        checkRadio('todo-settings-case-no', true);
+                }
+
+                if (settings.search.scope === 'current') {
+                        checkRadio('todo-settings-scope-file', true);
+                        checkRadio('todo-settings-scope-project', false);
+                } else {
+                        checkRadio('todo-settings-scope-file', false);
+                        checkRadio('todo-settings-scope-project', true);
+                }
+                $('#todo-settings-exclude-folders').val(settings.search.excludeFolders.join(', '));
+                $('#todo-settings-exclude-files').val(settings.search.excludeFiles.join(', '));
+        }
+
+        /**
+         * Split string by comma into array
+         * @return {Array}
+         */
+        function splitByComma(value) {
+                var result = [];
+
+                if (value === null || value === undefined) {
+                        return result;
+                }
+
+                if (value.trim().length > 0) {
+
+                        // Filtering empty string
+                        $.each(value.split(','), function (index, item) {
+                                if (item.trim().length > 0) {
+                                        result.push(item.trim());
+                                }
+                        });
+                }
+
+                return result;
+        }
+
+        /**
+         * Exposed method to show dialog.
+         */
+        exports.showDialog = function (settings, onSaveCallback) {
+                // Compile dialog template.
+                var compiledTemplate = Mustache.render(settingsDialogTemplate, {
+                        Strings: Strings
+                });
+
+                // Save dialog to variable.
+                dialog = Dialogs.showModalDialogUsingTemplate(compiledTemplate);
+
+                // Initialize dialog values.
+                initValues(settings);
+
+                // Open dialog.
+                dialog.done(function (buttonId) {
+                        // Save preferences if OK button was clicked.
+                        if (buttonId === 'ok') {
+                                var $dialog = dialog.getElement(),
+                                    newSettings = getValues($dialog);
+                                if (onSaveCallback) {
+                                        onSaveCallback(newSettings);
+                                }
+                        }
+                });
         };
-    }
-
-    /**
-     * check or uncheck radio button
-     */
-    function checkRadio(elementId, isChecked) {
-        $('#' + elementId).attr('checked', isChecked);
-    }
-
-    /**
-     * Initialize dialog values.
-     */
-    function initValues(settings) {
-        $('#todo-settings-regex-prefix').val(settings.regex.prefix);
-        $('#todo-settings-regex-suffix').val(settings.regex.suffix);
-        $('#todo-settings-tags').val(settings.tags.join(', '));
-        if (settings.case === true) {
-            checkRadio('todo-settings-case-yes', true);
-            checkRadio('todo-settings-case-no', false);
-        } else {
-            checkRadio('todo-settings-case-yes', false);
-            checkRadio('todo-settings-case-no', true);
-        }
-
-        if (settings.search.scope === 'current') {
-            checkRadio('todo-settings-scope-file', true);
-            checkRadio('todo-settings-scope-project', false);
-        } else {
-            checkRadio('todo-settings-scope-file', false);
-            checkRadio('todo-settings-scope-project', true);
-        }
-        $('#todo-settings-exclude-folders').val(settings.search.excludeFolders.join(', '));
-        $('#todo-settings-exclude-files').val(settings.search.excludeFiles.join(', '));
-    }
-
-    /**
-     * Split string by comma into array
-     * @return {Array}
-     */
-    function splitByComma(value) {
-        var result = [];
-
-        if (value === null || value === undefined) {
-            return result;
-        }
-
-        if (value.trim().length > 0) {
-
-            // Filtering empty string
-            $.each(value.split(','), function (index, item) {
-                if (item.trim().length > 0) {
-                    result.push(item.trim());
-                }
-            });
-        }
-
-        return result;
-    }
-
-    /**
-     * Exposed method to show dialog.
-     */
-    exports.showDialog = function (settings, onSaveCallback) {
-        // Compile dialog template.
-        var compiledTemplate = Mustache.render(settingsDialogTemplate, {
-            Strings: Strings
-        });
-
-        // Save dialog to variable.
-        dialog = Dialogs.showModalDialogUsingTemplate(compiledTemplate);
-
-        // Initialize dialog values.
-        initValues(settings);
-
-        // Open dialog.
-        dialog.done(function (buttonId) {
-            // Save preferences if OK button was clicked.
-            if (buttonId === 'ok') {
-                var $dialog = dialog.getElement(),
-                    newSettings = getValues($dialog);
-                if (onSaveCallback) {
-                    onSaveCallback(newSettings);
-                }
-            }
-        });
-    };
 });
--- nls/zh/Strings.js
+++ nls/zh/Strings.js
@@ -1,34 +1,34 @@
-define({
-    // EXTENSION.
-    EXTENSION_NAME: "任务列表",
+define( {
+        // EXTENSION.
+        EXTENSION_NAME: "任务列表",
 
-    // GENERAL.
-    YES: "是",
-    NO: "否",
+        // GENERAL.
+        YES: "是",
+        NO: "否",
 
-    // TOOLBAR.
-    EXPAND_ALL: "展开所有",
-    COLLAPSE_ALL: "收缩所有",
-    SHOW_OR_HIDE: "显示/隐藏",
+        // TOOLBAR.
+        EXPAND_ALL: "展开所有",
+        COLLAPSE_ALL: "收缩所有",
+        SHOW_OR_HIDE: "显示/隐藏",
 
-    // FILE DIALOG.
-    FILE_DIALOG_TITLE: "创建.todo文件？",
-    FILE_DIALOG_MESSAGE: "当前工程目录没有.todo文件。是否需要创建？",
+        // FILE DIALOG.
+        FILE_DIALOG_TITLE: "创建.todo文件？",
+        FILE_DIALOG_MESSAGE: "当前工程目录没有.todo文件。是否需要创建？",
 
-    // SETTINGS DIALOG.
-    SETTINGS_DIALOG_TITLE:              "Todo配置",
-    SETTINGS_DIALOG_REGEX_PREFIX:       "正则表达式前缀",
-    SETTINGS_DIALOG_REGEX_PREFIX_TITLE: "用于匹配注释头部的正则表达式，修改成不同的正则表达式可以支持不同的语言。",
-    SETTINGS_DIALOG_REGEX_SUFFIX:       "正则表达式后缀",
-    SETTINGS_DIALOG_REGEX_SUFFIX_TITLE: "用于匹配注释尾部的正则表达式，修改成不同的正则表达式可以支持不同的语言。",
-    SETTINGS_DIALOG_TAGS:               "标签",
-    SETTINGS_DIALOG_TAGS_TITLE:         "多个标签之间使用半角逗号分隔，标签名支持正则表达式。",
-    SETTINGS_DIALOG_CASE:               "大小写敏感",
-    SETTINGS_DIALOG_SEARCH_SCOPE:       "搜索范围",
-    SETTINGS_DIALOG_FILE:               "当前文件",
-    SETTINGS_DIALOG_PROJECT:            "当前工程",
-    SETTINGS_DIALOG_EXCLUDE_FOLDERS:    "要排除的文件夹",
-    SETTINGS_DIALOG_EXCLUDE_FOLDERS_TITLE:  "在搜索中需要排除的文件夹名，多个名字之间使用逗号分隔，可以填写完整的文件夹名，或者只是文件夹名的一部分。",
-    SETTINGS_DIALOG_EXCLUDE_FILES:          "要排除的文件",
-    SETTINGS_DIALOG_EXCLUDE_FILES_TITLE:    "在搜索中需要排除的文件名，多个名字之间使用逗号分隔，可以填写完整的文件名，或者只是文件名的一部分。比如.css。"
-});
+        // SETTINGS DIALOG.
+        SETTINGS_DIALOG_TITLE: "Todo配置",
+        SETTINGS_DIALOG_REGEX_PREFIX: "正则表达式前缀",
+        SETTINGS_DIALOG_REGEX_PREFIX_TITLE: "用于匹配注释头部的正则表达式，修改成不同的正则表达式可以支持不同的语言。",
+        SETTINGS_DIALOG_REGEX_SUFFIX: "正则表达式后缀",
+        SETTINGS_DIALOG_REGEX_SUFFIX_TITLE: "用于匹配注释尾部的正则表达式，修改成不同的正则表达式可以支持不同的语言。",
+        SETTINGS_DIALOG_TAGS: "标签",
+        SETTINGS_DIALOG_TAGS_TITLE: "多个标签之间使用半角逗号分隔，标签名支持正则表达式。",
+        SETTINGS_DIALOG_CASE: "大小写敏感",
+        SETTINGS_DIALOG_SEARCH_SCOPE: "搜索范围",
+        SETTINGS_DIALOG_FILE: "当前文件",
+        SETTINGS_DIALOG_PROJECT: "当前工程",
+        SETTINGS_DIALOG_EXCLUDE_FOLDERS: "要排除的文件夹",
+        SETTINGS_DIALOG_EXCLUDE_FOLDERS_TITLE: "在搜索中需要排除的文件夹名，多个名字之间使用逗号分隔，可以填写完整的文件夹名，或者只是文件夹名的一部分。",
+        SETTINGS_DIALOG_EXCLUDE_FILES: "要排除的文件",
+        SETTINGS_DIALOG_EXCLUDE_FILES_TITLE: "在搜索中需要排除的文件名，多个名字之间使用逗号分隔，可以填写完整的文件名，或者只是文件名的一部分。比如.css。"
+} );
