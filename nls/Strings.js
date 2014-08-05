6e815fd4dc290ca20510b8da9a9dadc8dd1be9de
Mikael Jorhult <mikael@jorhult.se>
2014-08-05 09:17:38 +0200

    French translation.
    Resolves #75.

--- nls/Strings.js
+++ nls/Strings.js
@@ -4,6 +4,7 @@ define( function( require, exports, module ) {
 	module.exports = {
 		root: true,
 		de: true,
+		fr: true,
 		it: true,
 		sv: true,
 		zh: true
---
+++ nls/fr/Strings.js
@@ -0,0 +1,24 @@
+define( {
+	// EXTENSION.
+	EXTENSION_NAME: "Todo",
+	
+	// GENERAL.
+	YES:    "Oui",
+	NO:     "Non",
+	OK:     "Ok",
+	CANCEL: "Annuler",
+	
+	// TOOLBAR.
+	CONFIGURE:     "Configurer",
+	EXPAND_ALL:    "Développer Tout",
+	COLLAPSE_ALL:  "Réduire Tout",
+	SHOW_OR_HIDE:  "Afficher/Masquer",
+	
+	// FILE DIALOG.
+	FILE_DIALOG_TITLE:   "Créer un fichier .todo ?",
+	FILE_DIALOG_MESSAGE: "Le dossier du projet en cours ne contient pas de fichier .todo . Voulez-vous en créer un ?",
+	
+	// SETTINGS DIALOG.
+	SETTINGS_DIALOG_TITLE:        "Paramètres de Todo",
+	SETTINGS_DIALOG_REGEX_PREFIX: "prefix RegExp"
+} );
