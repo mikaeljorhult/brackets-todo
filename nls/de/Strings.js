3fc1cb32719e63fc2fa5d18154a2ff1f5263efa0
Mikael Jorhult <mikael@jorhult.se>
2014-08-08 22:33:27 +0200

    Placeholders for missing string in translations.

--- nls/de/Strings.js
+++ nls/de/Strings.js
@@ -3,12 +3,36 @@ define( {
 	EXTENSION_NAME: "Todo",
 	
 	// GENERAL.
-	YES: "Ja",
-	NO:  "Nein",
+	YES:    "Ja",
+	NO:     "Nein",
+	//OK:     "Okay",
+	//CANCEL: "Cancel",
 	
 	// TOOLBAR.
-	CONFIGURE:     "Konfigurieren",
-	EXPAND_ALL:    "Alle ausklappen",
-	COLLAPSE_ALL:  "Alle einklappen",
-	SHOW_OR_HIDE:  "Anzeigen/verstecken"
+	CONFIGURE:    "Konfigurieren",
+	EXPAND_ALL:   "Alle ausklappen",
+	COLLAPSE_ALL: "Alle einklappen",
+	SHOW_OR_HIDE: "Anzeigen/verstecken",
+	//REFRESH:      "Refresh",
+	
+	// SETTINGS DIALOG.
+	//SETTINGS_DIALOG_TITLE:                 "Todo Settings",
+	//SETTINGS_DIALOG_REGEX:                 "Regular expression",
+	//SETTINGS_DIALOG_REGEX_PREFIX:          "Prefix",
+	//SETTINGS_DIALOG_REGEX_PREFIX_TITLE:    "Regular expression for matching beginning of comment. Modify it to support different language.",
+	//SETTINGS_DIALOG_REGEX_SUFFIX:          "Suffix",
+	//SETTINGS_DIALOG_REGEX_SUFFIX_TITLE:    "Regular expression for matching end of comment. Modify it to support different language.",
+	//SETTINGS_DIALOG_TAGS:                  "Tags",
+	//SETTINGS_DIALOG_TAGS_TITLE:            "Tag names separated by comma. Names may contain regular expressions.",
+	//SETTINGS_DIALOG_CASE:                  "Case sensitive",
+	//SETTINGS_DIALOG_SEARCH_SCOPE:          "Search scope",
+	//SETTINGS_DIALOG_FILE:                  "Current file",
+	//SETTINGS_DIALOG_PROJECT:               "Current project",
+	//SETTINGS_DIALOG_EXCLUDE:               "Exclude",
+	//SETTINGS_DIALOG_EXCLUDE_FOLDERS:       "Folders",
+	//SETTINGS_DIALOG_EXCLUDE_FOLDERS_TITLE: "Full or part of folder names, separated by commas, which should be excluded from search.",
+	//SETTINGS_DIALOG_EXCLUDE_FILES:         "Files",
+	//SETTINGS_DIALOG_EXCLUDE_FILES_TITLE:   "Full or part of file names, separated by commas, which should be excluded from search.",
+	//SETTINGS_DIALOG_SAVE_FILE:             "Save to .todo",
+	//SETTINGS_DIALOG_RESET:                 "Reset"
 } );
--- nls/fr/Strings.js
+++ nls/fr/Strings.js
@@ -9,16 +9,30 @@ define( {
 	CANCEL: "Annuler",
 	
 	// TOOLBAR.
-	CONFIGURE:     "Configurer",
-	EXPAND_ALL:    "Développer Tout",
-	COLLAPSE_ALL:  "Réduire Tout",
-	SHOW_OR_HIDE:  "Afficher/Masquer",
-	
-	// FILE DIALOG.
-	FILE_DIALOG_TITLE:   "Créer un fichier .todo ?",
-	FILE_DIALOG_MESSAGE: "Le dossier du projet en cours ne contient pas de fichier .todo . Voulez-vous en créer un ?",
+	CONFIGURE:    "Configurer",
+	EXPAND_ALL:   "Développer Tout",
+	COLLAPSE_ALL: "Réduire Tout",
+	SHOW_OR_HIDE: "Afficher/Masquer",
+	//REFRESH:      "Refresh",
 	
 	// SETTINGS DIALOG.
-	SETTINGS_DIALOG_TITLE:        "Paramètres de Todo",
-	SETTINGS_DIALOG_REGEX_PREFIX: "prefix RegExp"
+	SETTINGS_DIALOG_TITLE:                 "Paramètres de Todo",
+	//SETTINGS_DIALOG_REGEX:                 "Regular expression",
+	SETTINGS_DIALOG_REGEX_PREFIX:          "prefix RegExp",
+	//SETTINGS_DIALOG_REGEX_PREFIX_TITLE:    "Regular expression for matching beginning of comment. Modify it to support different language.",
+	//SETTINGS_DIALOG_REGEX_SUFFIX:          "Suffix",
+	//SETTINGS_DIALOG_REGEX_SUFFIX_TITLE:    "Regular expression for matching end of comment. Modify it to support different language.",
+	//SETTINGS_DIALOG_TAGS:                  "Tags",
+	//SETTINGS_DIALOG_TAGS_TITLE:            "Tag names separated by comma. Names may contain regular expressions.",
+	//SETTINGS_DIALOG_CASE:                  "Case sensitive",
+	//SETTINGS_DIALOG_SEARCH_SCOPE:          "Search scope",
+	//SETTINGS_DIALOG_FILE:                  "Current file",
+	//SETTINGS_DIALOG_PROJECT:               "Current project",
+	//SETTINGS_DIALOG_EXCLUDE:               "Exclude",
+	//SETTINGS_DIALOG_EXCLUDE_FOLDERS:       "Folders",
+	//SETTINGS_DIALOG_EXCLUDE_FOLDERS_TITLE: "Full or part of folder names, separated by commas, which should be excluded from search.",
+	//SETTINGS_DIALOG_EXCLUDE_FILES:         "Files",
+	//SETTINGS_DIALOG_EXCLUDE_FILES_TITLE:   "Full or part of file names, separated by commas, which should be excluded from search.",
+	//SETTINGS_DIALOG_SAVE_FILE:             "Save to .todo",
+	//SETTINGS_DIALOG_RESET:                 "Reset"
 } );
