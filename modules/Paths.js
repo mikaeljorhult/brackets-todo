4be4307354c962f091816dd368ae4b37a0af8424
Mikael Jorhult <mikael@jorhult.se>
2014-08-09 13:11:00 +0200

    Comment Paths module.

--- modules/Paths.js
+++ modules/Paths.js
@@ -8,22 +8,32 @@ define( function() {
 		projectRoot,
 		todoFile;
 	
+	/**
+	 * Return path to current project root.
+	 */
 	function getProjectRoot() {
 		projectRoot = projectRoot || ProjectManager.getProjectRoot().fullPath;
 		
 		return projectRoot;
 	}
 	
+	/**
+	 * Return path to .todo file in current project root.
+	 */
 	function getTodoFile() {
 		todoFile = todoFile || getProjectRoot() + '.todo';
 		
 		return todoFile;
 	}
 	
+	/**
+	 * Make a full path relative to project root.
+	 */
 	function makeRelative( path ) {
 		return path.replace( getProjectRoot(), '' );
 	}
 	
+	// Return module.
 	return {
 		makeRelative: makeRelative,
 		projectRoot: getProjectRoot,
