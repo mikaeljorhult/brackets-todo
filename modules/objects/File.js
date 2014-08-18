d84758956b687e8328626e0e6ddbaab6c57e4baf
Mikael Jorhult <mikael@jorhult.se>
2014-08-18 23:05:25 +0200

    Add file object.

---
+++ modules/objects/File.js
@@ -0,0 +1,69 @@
+define( function( require ) {
+	'use strict';
+	
+	// Get dependencies.
+	var ProjectManager = brackets.getModule( 'project/ProjectManager' ),
+		
+		// Extension modules.
+		Paths = require( 'modules/Paths' );
+	
+	// Define file object.
+	function File( file ) {
+		// Use object properties if one was supplied.
+		if ( typeof( file ) === 'object' ) {
+			this._name = file.name;
+			this._path = file.path;
+			this._todos = file.todos;
+		} else {
+			this._name = '';
+			this._path = '';
+			this._todos = [];
+		}
+	}
+	
+	// Methods handling file name.
+	File.prototype.name = function( name ) {
+		// Return file name if no new name is supplied.
+		if ( name === undefined ) {
+			return this._name;
+		}
+		
+		// Set file name if one is supplied.
+		this._name = name;
+	}
+	
+	File.prototype.path = function( path ) {
+		// Return path if no new name is supplied.
+		if ( path === undefined ) {
+			return this._path;
+		}
+		
+		// Set path if one is supplied.
+		this._path = path;
+		
+		// Set file name relative to project root.
+		this._name = Paths.makeRelative( path );
+	}
+	
+	// Methods handling todos.
+	File.prototype.todos = function( todos ) {
+		// Return path if no new name is supplied.
+		if ( todos === undefined ) {
+			return this._todos;
+		}
+		
+		// Set todos if supplied.
+		this._todos = todos;
+	}
+	
+	File.prototype.addTodo = function( todo ) {
+		this._todos.push( todo );
+	}
+	
+	File.prototype.clearTodos = function() {
+		this._todos = [];
+	}
+	
+	// Return object.
+	return File;
+} );
