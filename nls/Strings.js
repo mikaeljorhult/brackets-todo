5485a17ec4caf707c8d18f1ef2459a2c3edbf221
Dis Shishkov <dis@velocitymarkets.net>
2014-09-04 15:02:35 +0400

    added Russian language support

--- nls/Strings.js
+++ nls/Strings.js
@@ -8,6 +8,7 @@ define( function( require, exports, module ) {
 		fr: true,
 		gl: true,
 		it: true,
+        ru: true,
 		sv: true,
 		uk: true,
 		zh: true
---
+++ nls/ru/Strings.js
@@ -0,0 +1,40 @@
+define( {
+	// EXTENSION.
+	EXTENSION_NAME: "Todo",
+	
+	// GENERAL.
+	YES:    "Да",
+	NO:     "Нет",
+	OK:     "Хорошо",
+	CANCEL: "Отмена",
+	
+	// TOOLBAR.
+	CONFIGURE:    "Настроить",
+	EXPAND_ALL:   "Развернуть все",
+	COLLAPSE_ALL: "Свернуть все",
+	SHOW_OR_HIDE: "Показать/Скрыть",
+	REFRESH:      "Обновить",
+	
+	// SETTINGS DIALOG.
+	SETTINGS_DIALOG_TITLE:                 "Настройки Todo",
+	SETTINGS_DIALOG_REGEX:                 "Регулярное выражение",
+	SETTINGS_DIALOG_REGEX_PREFIX:          "Префикс",
+	SETTINGS_DIALOG_REGEX_PREFIX_TITLE:    "Регулярное выражение, соответствующее началу комментария. Измените его для поддержки различных языков.",
+	SETTINGS_DIALOG_REGEX_SUFFIX:          "Суффикс",
+	SETTINGS_DIALOG_REGEX_SUFFIX_TITLE:    "Регулярное выражение, соответствующее окончанию комментария. Измените его для поддержки различных языков.",
+	SETTINGS_DIALOG_TAGS:                  "Метки",
+	SETTINGS_DIALOG_TAGS_TITLE:            "Названия меток, разделённые запятой. Названия могут содержать регулярные выражения.",
+	SETTINGS_DIALOG_CASE:                  "Чувствительность к регистру",
+	SETTINGS_DIALOG_SEARCH_SCOPE:          "Область поиска",
+	SETTINGS_DIALOG_FILE:                  "Текущий фаил",
+	SETTINGS_DIALOG_PROJECT:               "Текущий проект",
+	SETTINGS_DIALOG_EXCLUDE:               "Исключить",
+	SETTINGS_DIALOG_EXCLUDE_FOLDERS:       "Папки",
+	SETTINGS_DIALOG_EXCLUDE_FOLDERS_TITLE: "Полные или частичные имена папок, разделённые запятыми, которые должны быть исключены из поиска.",
+	SETTINGS_DIALOG_EXCLUDE_FILES:         "Файлы",
+	SETTINGS_DIALOG_EXCLUDE_FILES_TITLE:   "Полные или частичные имена файлов, разделённые запятыми, которые должны быть исключены из поиска.",
+	SETTINGS_DIALOG_SORTING_FILTERING:     "Сортировка и фильтрация",
+	SETTINGS_DIALOG_SORT_DONE:             "Переместить выполненные задачи вниз списка.",
+	SETTINGS_DIALOG_SAVE_FILE:             "Сохранить в .todo",
+	SETTINGS_DIALOG_RESET:                 "Сбросить"
+} );
