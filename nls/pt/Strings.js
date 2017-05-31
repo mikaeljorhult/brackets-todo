81fa749ec9e5ed696b4a05f0d256f7e85c144ede
Ndot <Ndot@users.noreply.github.com>
2017-05-31 17:53:02 +0100

    Added Portuguese
    Since when using PT language it will default to the other PT language, this will also work for BR.

---
+++ nls/pt/Strings.js
@@ -0,0 +1,41 @@
+define({
+  // EXTENSION.
+  EXTENSION_NAME: 'Todo',
+
+  // GENERAL.
+  YES: 'Sim',
+  NO: 'Não',
+  OK: 'Aceitar',
+  CANCEL: 'Cancelar',
+    
+  // TOOLBAR.
+  CONFIGURE: 'Configurações',
+  EXPAND_ALL: 'Expandir todos',
+  COLLAPSE_ALL: 'Fechar todos',
+  SHOW_OR_HIDE: 'Mostrar/Ocultar',
+  REFRESH: 'Recarregar',
+
+  // SETTINGS DIALOG.
+  SETTINGS_DIALOG_TITLE: 'Todo Configurações',
+  SETTINGS_DIALOG_REGEX: 'Expressões Regulares',
+  SETTINGS_DIALOG_REGEX_PREFIX: 'Prefixo',
+  SETTINGS_DIALOG_REGEX_PREFIX_TITLE: 'Expressão Regular para corresponder com o inicio do comentário. Modifique para suportar outras linguagens de programação.',
+  SETTINGS_DIALOG_REGEX_SUFFIX: 'Sufixo',
+  SETTINGS_DIALOG_REGEX_SUFFIX_TITLE: 'Expressão Regular para corresponder com o fim do comentário. Modifique para suportar outras linguagens de programação.',
+  SETTINGS_DIALOG_TAGS: 'Etiquetas',
+  SETTINGS_DIALOG_TAGS_TITLE: 'Nome de etiquetas separadas por virgulas. Os nomes podem conter Expressões Regulares.',
+  SETTINGS_DIALOG_CASE: 'Só Maiúsculas',
+  SETTINGS_DIALOG_SEARCH_SCOPE: 'Âmbito da pesquisa',
+  SETTINGS_DIALOG_FILE: 'Ficheiro corrente',
+  SETTINGS_DIALOG_PROJECT: 'Projecto corrente',
+  SETTINGS_DIALOG_EXCLUDE: 'Excluir',
+  SETTINGS_DIALOG_EXCLUDE_FOLDERS: 'Pastas',
+  SETTINGS_DIALOG_EXCLUDE_FOLDERS_TITLE: 'Nome ou parte do nome de pastas, separadas por virgulas, para serem excluídos da busca.',
+  SETTINGS_DIALOG_EXCLUDE_FILES: 'Ficheiros',
+  SETTINGS_DIALOG_EXCLUDE_FILES_TITLE: 'Nome ou parte do nome de ficheiros, separados por virgulas, para serem excluídos da busca.',
+  SETTINGS_DIALOG_SORTING_FILTERING: 'Ordenar e filtrar',
+  SETTINGS_DIALOG_SORT_DONE: 'Mover tarefas concluídas para o fim da lista.',
+  SETTINGS_DIALOG_HIDE_DONE: 'Esconder tarefas concluídas.',
+  SETTINGS_DIALOG_SAVE_FILE: 'Salvar para o ficheiro .todo',
+  SETTINGS_DIALOG_RESET: 'Recarregar'
+});
