mikaeljorhult/brackets-todo

commit 018a0201181b02a6fb5006de225164ad11bdb293

Issues #139 Fix
Fixed bug where //[TAG] wasn't displaying in the TODO dialog by updating the suffix regex

modules/Defaults.js
@@ -10,7 +10,7 @@ define(function () {
   var defaultSettings = {
     regex: {
       prefix: '(?:<!--|\\/\\*|\\/\\/|#|--) *@?(',
-      suffix: '):? *(.*?) ?(?=-->|\\*/|\\n|$)'
+      suffix: '):? *(.*?) ?(?=\/\*|-->|\*\/|\r\n|\n|\r|$)'
     },
     tags: ['TODO', 'NOTE', 'FIX ?ME', 'CHANGES', 'FUTURE'],
     case: false,

commit 57e6dac56783b3a092d0422d0ff6a288ab16c751

Fix unescaped backslashes
Commit 018a020 broke the extension causing the popup to not open up at all.

See Issue #142

modules/Defaults.js
@@ -10,7 +10,7 @@ define(function () {
   var defaultSettings = {
     regex: {
       prefix: '(?:<!--|\\/\\*|\\/\\/|#|--) *@?(',
-      suffix: '):? *(.*?) ?(?=\/\*|-->|\*\/|\r\n|\n|\r|$)'
+      suffix: '):? *(.*?) ?(?=\\/\\*|-->|\\*\\/|\\r\\n|\\n|\\r|$)'
     },
     tags: ['TODO', 'NOTE', 'FIX ?ME', 'CHANGES', 'FUTURE'],
     case: false,